8dc93d9a22e500123549c8211f3da1f8ab13a286
Anwesha Bhadury <anweshabhadury5@gmail.com>
2026-05-02 22:27:35 +0530

    Added Transition Marquee

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Hero from "@/src/components/sections/Home/HomeHero"
 import Project from "@/src/components/sections/Home/ProjectSection"
 import Footer from "@/src/components/shared/footer"
 import About from "@/src/components/sections/Home/About"
+import TransitionSection from "@/src/components/sections/Home/TransitionSection.jsx"
 
 export default function Page() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TransitionSection/>
       <About/>
       <Project/>
       <Footer/>
